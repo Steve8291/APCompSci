c9772a6fb421ccc1c5af3e9cc3a1f312ef45c592
Steve <hambling8@gmail.com>
2023-09-22 09:49:09 -0400

    Update 03_numbers.py

--- 03_numbers.py
+++ 03_numbers.py
@@ -24,4 +24,19 @@ print(f"This is an int: {num1}")
 # Floating Point Numbers contain a decimal place.
 my_float = 5.
 print(f"This is a float: {my_float}")
-print(f"Division will produce a float: {num2 / num2}")
+print(f"Division will always produce a float: {num2 / num2}")
+
+print("\n________ Large Numbers ________")
+universe_age = 14_000_000_000  # Python ignores underscores in a number.
+print(f"You can use underscores to make large numbers more readable in your code: {universe_age}")
+
+print("\n________ Multiple Assignment ________")
+x, y, z = 1, 2, "my string"
+print(f"You can assign multiple variables on one line. x={x}, y={y}, z={z}")
+
+print("\n________ Constants ________")
+# Constants are variables that will never change in your program.
+# The convention is to capitalize constants.
+PI = 3.14
+SCHOOL_NAME = "Columbus East H.S."
+print(f"Constants are capitalized. SCHOOL_NAME = {SCHOOL_NAME}, PI = {PI}")
