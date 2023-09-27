2a7b4c56c119f9e3350eb9f08c685ecfe5f58fd2
Steve <hambling8@gmail.com>
2023-09-27 10:25:52 -0400

    Create 08_for_loops.py

---
+++ 08_for_loops.py
@@ -0,0 +1,13 @@
+
+num = 3
+
+
+for x in range(num):
+    print(x)
+    print(f"This is the {x} time through the loop")
+
+name_list = ['something', 'something else']
+for name in name_list:
+    print(name)
+
+
