43a09720bccc7cff9cd4e7264b452c7c2c80f739
Steve <hambling8@gmail.com>
2024-02-01 11:13:36 -0500

    Update 00_tkinter_template.py

--- Tkinter/00_tkinter_template.py
+++ Tkinter/00_tkinter_template.py
@@ -10,17 +10,21 @@ from tkinter.messagebox import showinfo
 root = tk.Tk()
 root.title("Tkinter Widget Demo")
 
+
 # Frame widget container
 window = ttk.Frame()
 window.grid(padx=20, pady=20)
 
+
 # Label widget
-label = ttk.Label(window, text="This is my Label!")
+label = ttk.Label(window, text="This is my Label. It spans 2 columns!")
+
 
 # Entry widget
 entry_var = tk.StringVar()
 entry = ttk.Entry(window, textvariable=entry_var)
 entry.focus()  # Puts the cursor into the entry field
+
 
 # Button widget
 def on_button():
@@ -30,16 +34,24 @@ def on_button():
 button = ttk.Button(window, text="My Button", command=on_button)
 
 
-# Messagebox showinfo widget
-def combobox_showinfo(event):
-    showinfo(title="Combobox Selected!", message=f"You selected {combobox_var.get()}")
-    
-    print(combobox_var.get())
-
-# Combobox widget:
-def combobox_changed(event):
+# Button widget with lambda function
+# Allows you to pass parameters to a function
+def on_lambutton(event):
     print(event)
+    print(f"The combobox is: {combobox_var.get()}")
 
+event_type = "My specialevent"
+lambutton = ttk.Button(window, text="LamButton", command=lambda: on_lambutton(event_type))
+
+
+# Combobox showinfo widget
+# This is triggered by changing the combobox widget below
+def combobox_changed(event):
+    print(combobox_var.get())
+    showinfo(title="Combobox Selected!", message=f"You selected {combobox_var.get()}")
+
+
+# Combobox widget
 combobox_list = ('value 1', 'value 2', 'value 3')
 combobox_var = tk.StringVar()
 combobox = ttk.Combobox(window, textvariable=combobox_var)
@@ -51,9 +63,10 @@ combobox.bind('<<ComboboxSelected>>', combobox_changed)
 
 
 # Grid Widgets into Frame
-label.grid(row=0, column=0)
+label.grid(row=0, column=0, columnspan=2)
 entry.grid(row=1, column=0)
-button.grid(row=5, column=1)
-combobox.grid(row=2, column=1)
+button.grid(row=2, column=0)
+combobox.grid(row=1, column=1)
+lambutton.grid(row=2, column=1)
 
 root.mainloop()
